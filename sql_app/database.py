38a071c6814cdd784fe9d4ba496782beba2f7478
Folkert Stijnman <folkertstijnman@gmail.com>
2021-11-01 21:25:42 +0100

    database py clean

--- sql_app/database.py
+++ sql_app/database.py
@@ -5,9 +5,7 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
