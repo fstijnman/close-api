026602cc415313c5fc44285c8bfefaca48c9fe04
Folkert Stijnman <folkertstijnman@gmail.com>
2021-11-16 20:10:29 +0100

    all origins

--- sql_app/main.py
+++ sql_app/main.py
@@ -12,12 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
